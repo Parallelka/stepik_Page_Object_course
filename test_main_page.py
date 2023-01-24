8f76ce0ce146e3cd979106e0f1e36c5cc9649565
Parallelka <yevali@i.ua>
2023-01-24 16:35:27 +0200

    Import class MainPage

--- test_main_page.py
+++ test_main_page.py
@@ -5,6 +5,14 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.chrome.options import Options
 import time
 
+from .pages.main_page import MainPage
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()                      # открываем страницу
+    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+    
 #def test_guest_can_go_to_login_page(browser):
     #link = "http://selenium1py.pythonanywhere.com/"
     #browser.get(link)
