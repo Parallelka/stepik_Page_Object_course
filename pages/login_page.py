d286db1afc2fdfa5a2e879825cddeb3ee195897b
Parallelka <yevali@i.ua>
2023-01-25 23:43:55 +0200

    Implementation LoginPage

---
+++ pages/login_page.py
@@ -0,0 +1,30 @@
+from .base_page import BasePage
+from selenium.webdriver.common.by import By
+from .locators import LoginPageLocators
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+
+        register_form = self.browser.find_element(*LoginPageLocators.REGISTER_FORM)
+        register_form.click()
+
+        login_url = self.browser.find_element(*LoginPageLocators.LOGIN_URL)
+        login_url.click()
+
+        login_form = self.browser.find_element(*LoginPageLocators.LOGIN_FORM)
+        login_form.click()
+
+       # def should_be_login_page(self):
+        #    self.should_be_login_url()
+        #    self.should_be_login_form()
+         #   self.should_be_register_form()
+
+    def should_be_login_form(self):
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
+
+    def should_be_register_form(self):
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
+
+    def should_be_login_url(self):
+        assert "login" in browser.current_url, "Login is not presented"  # проверка того, что в текущем url содержится строка login
+
