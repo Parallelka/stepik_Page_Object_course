8f910085f450dadae7371d710f0b0a2d4d1d1d4b
Parallelka <yevali@i.ua>
2023-01-25 15:12:57 +0200

    Add assert in main_page (method - should_be_login_lin, try/except in base_page (method - is_element_present), timeout in __init____init__

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,5 @@
 from pages.main_page import MainPage
+from selenium.common.exceptions import NoSuchElementException
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
@@ -11,3 +12,6 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+def should_be_login_link(self):
+    assert self.is_element_present(By.CSS_SELECTOR, "#login_link_invalid"), "Login link is not presented"
