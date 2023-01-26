f5f1c7fb6be539d2013054656ea9db74b7e35a2e
Parallelka <yevali@i.ua>
2023-01-26 23:42:21 +0200

    Add alert in go_to_login_page

--- test_main_page.py
+++ test_main_page.py
@@ -8,7 +8,7 @@ def test_guest_can_go_to_login_page(browser):
     page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
-    login_page.should_be_login_form()
+    #login_page.should_be_login_form()
     #login_page.should_be_register_form()
     #login_page.should_be_login_url()
 
